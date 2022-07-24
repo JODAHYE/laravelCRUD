8d0702991283b0a739f5d209bb928fa9334329b9
dahye <dahye8043@gmail.com>
2022-07-24 14:48:39 +0900

    chore : 게시글 번호 추가

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
 <div class='bg-indigo-500'>
-  <ul class='flex justify-center items-center gap-8 text-3xl py-8 text-white'>
-    <li>
+  <ul class='flex justify-center items-center gap-8 text-3xl py-8'>
+    <li class='text-white ease-in duration-300 hover:text-black'>
       <a href='/'>LIST</a>
     </li> 
-    <li>
+    <li class='text-white ease-in duration-300 hover:text-black'>
       <a href='/create'>CREATE</a>
     </li>
   </ul>
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -7,12 +7,14 @@
 <h1>게시글 리스트</h1>
 <table class='w-full'>
     <tr>
+        <th>번호</th>
         <th class='w-3/5'>제목</th>
         <th>작성자</th>
         <th>작성일</th>
     </tr>
     @foreach($posts as $post)
     <tr class='cursor-pointer hover:bg-gray-100' onClick="location.href='/show/{{ $post->id }}'">
+        <td>{{ $post->id }}</td>
         <td>{{ $post->title }}</td>
         <td>{{ $post->writer }}</td>
         <td>{{ $post->created_at }}</td>
