17bd954669f374829894d0e41f71c6a30beb07f2
dahye <dahye8043@gmail.com>
2022-07-24 15:24:00 +0900

    chore : migration file 생성

--- database/migrations/2022_07_24_151433_create_posts_table.php
+++ database/migrations/2022_07_24_151433_create_posts_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->title();
-            $table->writer();
-            $table->content();
+            $table->string('title');
+            $table->string('writer');
+            $table->string('content');
             $table->timestamps();
         });
     }
